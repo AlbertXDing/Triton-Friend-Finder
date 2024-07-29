f721816e06e49f1def0f78b4a9e7fb7218d2577f
Albert Ding <axding@ucsd.edu>
2024-07-28 20:50:23 -0700

    testing config

--- Users/src/main/java/com/trition_friend_finder/controllers/UserController.java
+++ Users/src/main/java/com/trition_friend_finder/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,7 @@ public class UserController {
         return new ResponseEntity<>(userService.singleUser(username),HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody Map<String,String> payload) {
         return new ResponseEntity<>(userService.createUser(
